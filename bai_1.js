593b46914f70baa8dee7520bf313cfdb7bb13b20
tuanle290901 <tuanle290901@gmail.com>
2023-03-30 11:00:19 +0700

    nopbai

--- bai_1.js
+++
@@ -1,5 +0,0 @@
-const arr = [1, 2, 3, 4, 5];
-console.log(tryRemoveFromArray(arr, 2)); // Kết quả: [1, 2, 4, 5]
-console.log(tryRemoveFromArray(arr, 5)); // Kết quả: [1, 2, 3, 4, 5]
-console.log(tryRemoveFromArray(arr, -1)); // Kết quả: [1, 2, 3, 4, 5]
-console.log(tryRemoveFromArray(arr, 'a')); // Kết quả: [1, 2, 3, 4, 5]
